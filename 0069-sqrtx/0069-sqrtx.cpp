27324aa707ea58e8265e068262edbd1001187887
ajaditya00 <118525667+ajaditya00@users.noreply.github.com>
2024-04-19 20:13:11 +0530

    Time: 5 ms (35.3%), Space: 7.5 MB (5.86%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+    // double n = x / 2.0;
+    // cout<<n;
+    // while (abs(n * n - x) >0.1) {
+    //     n= (n + x / n) / 2.0;
+    //     cout<<n<<endl;
+    // }
+    // return n;
+    // }
+    return sqrt(x);
+    }
+};
