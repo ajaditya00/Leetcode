a9af3af77c323a5baeeca81612cc49d72c777227
ajaditya00 <118525667+ajaditya00@users.noreply.github.com>
2024-11-09 13:48:11 +0530

    Time: 1843 ms (5.03%), Space: 7.4 MB (50.55%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -2,14 +2,13 @@ class Solution {
 public:
     bool isPerfectSquare(int num) {
         long long i=1;
-        long long  prod=1;
+        long long prod=1;
+        bool ok=true;
         if(num==1) return true;
         while(i<num){
             prod=i*i;
             i++;
-            if(prod==num){
-            return true;
-            }
+            if(prod==num)return true;
         }
         return false;
     }
