b1262e1c6f1eed6f0b1fa788a70cd8b936e778c2
ajaditya00 <118525667+ajaditya00@users.noreply.github.com>
2024-11-20 23:29:34 +0530

    Time: 0 ms (100%), Space: 10.6 MB (85.74%) - LeetHub

--- 1503-reducing-dishes/1503-reducing-dishes.cpp
+++ 1503-reducing-dishes/1503-reducing-dishes.cpp
@@ -6,7 +6,7 @@ public:
         int suf[n];
         suf[n-1]=satisfaction[n-1];
         for(int i=n-2;i>=0;i--){
-            suf[i]=suf[i+1]+ satisfaction[i];
+            suf[i]=suf[i+1]+satisfaction[i];
         }
         int idx=-1;
         for(int i=0;i<n;i++){
@@ -16,12 +16,12 @@ public:
             }
         }
         if(idx==-1)return 0;
+        int m=0;
         int x=1;
-        int maxsum=0;
         for(int i=idx;i<n;i++){
-            maxsum+=(satisfaction[i]*x);
+            m+=(satisfaction[i]*x);
             x++;
         }
-        return maxsum;
+        return m;
     }
 };
