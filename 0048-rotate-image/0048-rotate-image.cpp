59c1989320a6ff71e7d274aaaaafde684d32425b
ajaditya00 <118525667+ajaditya00@users.noreply.github.com>
2024-11-23 00:11:50 +0530

    Time: 0 ms (100%), Space: 9 MB (12.46%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    //Transpose Function 
-    void transpose(vector<vector<int>>& m){
-        int n=m.size();
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<i;j++)
-            swap(m[i][j],m[j][i]);
-        }
-    }
     void rotate(vector<vector<int>>& matrix) {
-        transpose(matrix);
-        for(int i=0;i<matrix.size();i++){
-            reverse(matrix[i].begin(),matrix[i].end());
+        int n=matrix.size();
+        vector<vector<int>>temp(n, vector<int>(n,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                temp[j][n-i-1]=matrix[i][j];
+            }
         }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                matrix[i][j]=temp[i][j];
+            }
+        }
+        
     }
 };
