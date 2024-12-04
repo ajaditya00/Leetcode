e94e8433efcc931f211e319462a175997d991e43
ajaditya00 <118525667+ajaditya00@users.noreply.github.com>
2024-12-04 16:24:20 +0530

    Time: 0 ms (100%), Space: 12.7 MB (9.52%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -5,17 +5,17 @@ public:
             return true;
         if (p == NULL && q != NULL)
             return false;
-        if (p!= NULL && q == NULL)
+        if (p != NULL && q == NULL)
             return false;
-
         if (p->val != q->val)
             return false;
-        bool ok = isSameTree(p->left, q->left);
-        if (ok == false)
+        bool lst = isSameTree(p->left, q->left);
+        if (lst == false)
             return false;
-        bool ok1 = isSameTree(p->right, q->right);
-        if (ok1 == false)
+        bool rst = isSameTree(p->right, q->right);
+        if (rst == false)
             return false;
+
         return true;
     }
 };
