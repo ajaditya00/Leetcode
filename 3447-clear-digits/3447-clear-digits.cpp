0490d3b6d4c7194ca1e66be287d759af0f7c2c2c
ajaditya00 <118525667+ajaditya00@users.noreply.github.com>
2025-02-10 20:19:42 +0530

    Time: 0 ms (100%), Space: 8.4 MB (87.14%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string ans="";
+        for(int i=0;i<s.length();i++){
+            if(s[i]>='0' && s[i]<='9' && !ans.empty()){
+                ans.pop_back();
+            }
+            else ans+= s[i];
+        }
+        return ans;
+    }
+};
