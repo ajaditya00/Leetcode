3b7e1b10e1c37b61a924f4dbc1e3bf3764166a7a
ajaditya00 <118525667+ajaditya00@users.noreply.github.com>
2025-02-14 10:41:18 +0530

    Time: 907 ms (25.99%), Space: 78.2 MB (43.27%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.cpp
@@ -1,12 +1,7 @@
 class ProductOfNumbers {
 public:
     vector<int>nums;
-    int n; 
-    ProductOfNumbers() {
-        nums.clear();
-        n=0;
-    }
-    
+    int n;     
     void add(int num) {
         nums.push_back(num);
     }
