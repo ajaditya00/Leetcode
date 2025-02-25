87f1659c78ac6464725ea810cb27e640113f1917
ajaditya00 <118525667+ajaditya00@users.noreply.github.com>
2025-02-25 13:00:28 +0530

    Time: 0 ms (100%), Space: 8.6 MB (17.15%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
-
     int climbStairs(int n) {
-       // vector<vector<int>>dp(n);
         vector<int>dp(n+1,0);
         dp[0]=1;
         dp[1]=1;
@@ -10,6 +8,5 @@ public:
             dp[i]=dp[i-1]+dp[i-2];
         }
         return dp[n];
-        
     }
 };
