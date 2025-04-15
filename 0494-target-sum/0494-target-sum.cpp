8f9b2a98526555de5383b7e75006a2c1dc36afbe
ajaditya00 <118525667+ajaditya00@users.noreply.github.com>
2025-04-16 00:53:55 +0530

    Time: 18 ms (51.38%), Space: 25.5 MB (30.39%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
-    int ways(int i,vector<int>&nums,int t){
+    int sum=0;
+    int ways(int i,vector<int>&nums,int res,int t,vector<vector<int>>&dp){
         if(i==nums.size()){
-            if(t==0) return 1;
+            if(res==t) return 1;
             else return 0;
         }
-        int take=ways(i+1,nums,t+nums[i]);
-        int nottake=ways(i+1,nums,t-nums[i]);
-        return (take + nottake);
+        if(dp[i][res+sum]!=-1) return dp[i][res+sum];
+        int take=ways(i+1,nums,res+nums[i],t,dp);
+        int nottake=ways(i+1,nums,res-nums[i],t,dp);
+        return dp[i][res+sum] =(take + nottake);
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return ways(0,nums,target);
+        sum = accumulate(nums.begin(), nums.end(), 0);
+        int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(2*sum+1,-1));
+        return ways(0,nums,0,target,dp);
     }
 };
